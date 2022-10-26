de4e14627311cb74ee57a41a453c668f28181107
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-10-26 08:29:25 +0300

    mass assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+
+# susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+# susan = Person.new(susan_attributes)
+# susan.name      # => "Susan"
+# susan.height    # => "5'11""
+# puts susan.eye_color # => "Green"
+# puts susan.itself
